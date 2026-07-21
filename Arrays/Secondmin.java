b974cb0811707f1721bb0067d660872fdafe5001
Maruthigowda07 <ntrmaruthi72@gmail.com>
2026-07-21 18:56:48 +0530

    Implement Second Minimum finder in Java

---
+++ Arrays/Secondmin.java
@@ -0,0 +1,24 @@
+public class SecondMinimum {
+
+    public static void main(String[] args) {
+
+        int[] a = {8, 2, 6, 1, 9, 3};
+
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+
+        for (int i = 0; i < a.length; i++) {
+
+            if (a[i] < min) {
+                secondMin = min;
+                min = a[i];
+            }
+            else if (a[i] < secondMin && a[i] != min) {
+                secondMin = a[i];
+            }
+        }
+
+        System.out.println("Minimum = " + min);
+        System.out.println("Second Minimum = " + secondMin);
+    }
+}
