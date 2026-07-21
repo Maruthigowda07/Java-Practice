ee8e10315ab4564dd6958a132180f089674982f3
Maruthigowda07 <ntrmaruthi72@gmail.com>
2026-07-21 11:16:32 +0530

    Add files via upload

---
+++ Insertionsort.java
@@ -0,0 +1,30 @@
+package sortings;
+
+import java.util.Arrays;
+
+public class Insertionsort {
+
+	public static void main(String[] args) {
+		  int[] a= {2,4,3,1,5,5,6,};
+		  sort(a);
+		  System.out.println(Arrays.toString(a));
+	}
+
+	public static void sort(int[] a)
+	{
+		for(int i=1;i<a.length;i++)
+		{
+			int key=a[i];
+			int j=i-1;
+		
+			while(j>=0&&a[j]>key)
+			{
+			    a[j+1]=a[j];
+			    j--;
+			}
+			a[j+1]=key;
+		} 
+	}
+}
+
+
