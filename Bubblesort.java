a309892630954b10c2aca1f176ffd48bcc0150e8
Maruthigowda07 <ntrmaruthi72@gmail.com>
2026-07-21 11:16:50 +0530

    Add files via upload

---
+++ Bubblesort.java
@@ -0,0 +1,26 @@
+package sortings;
+import java.util.Arrays;
+public class Bubblesort {
+
+	public static void main(String[] args) {
+		  int[] a= {2,6,3,2,6,2,7};
+		  sort(a);
+		  System.out.println(Arrays.toString(a));
+	}
+
+	 public static void sort(int[] a)
+	 {
+		 for(int i=0;i<a.length;i++)
+		 {
+			 for(int j=0;j<a.length-1-i;j++)
+			 {
+				 if(a[j]>a[j+1])
+				 {
+					 int temp=a[j];
+					 a[j]=a[j+1];
+					 a[j+1]=temp;
+				 }
+			 }
+		 }
+	 }
+}
