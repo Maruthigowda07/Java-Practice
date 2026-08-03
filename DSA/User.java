578ae1ac9105b6cc297ac108302aaaa412b52f7f
Maruthi <ntrmaruthi72@gmail.com>
2026-08-03 17:03:11 +0530

    Add files via upload

---
+++ DSA/User.java
@@ -0,0 +1,46 @@
+package DSA;
+
+public class User {
+
+	public static void main(String[] args) {
+
+         ArrayList a=new ArrayList();
+         a.add(3);
+         a.add(1);
+         a.add(30);
+         a.add(40);
+         a.add(50);
+         a.add(60);
+         a.add(70);
+         a.add(80);
+        // a.remove(5);
+         
+      //  a.addIndex(2, 8);
+         
+         //for(int i=0;i<a.size();i++)
+        // {
+
+         //System.out.println(a.get(i));
+         
+        //System.out.println(a.get(1));
+         
+         //ystem.out.println(a.isEmpty());
+        //System.out.println(a.size());
+         
+         
+         //for(int i1=0;i1<=a.size();i1++)
+         //{
+         //System.out.println(a.get(i1));
+         
+         //System.out.println(a.contains(55));
+         a.set(2, 10000);
+         for(int i=0;i<=a.size();i++)
+         {
+         System.out.println(a.get(i));
+            
+
+	}
+	}
+}
+
+
